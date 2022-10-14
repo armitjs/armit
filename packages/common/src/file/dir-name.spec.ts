bbe8a8356f95fbd6c9851789215ada85f77a5939
tianyingchun <981919332@qq.com>
2022-10-14 14:09:09 +0800

    :truck: chore: update CI workflow

--- packages/common/src/file/dir-name.spec.ts
+++ packages/common/src/file/dir-name.spec.ts
@@ -3,6 +3,6 @@ import { getDirname } from './dir-name.js';
 describe('dir-name.mts', () => {
   it('should correct dynamic determined __dirname', () => {
     const result = getDirname(import.meta.url);
-    expect(result.includes('packages/helpers/src')).toBe(true);
+    expect(result.includes('packages/common/src')).toBe(true);
   });
 });
