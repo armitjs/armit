0b8fafa72aeaad0f74a4f864c533df9b890997de
tianyingchun <tianyingchun@outlook.com>
2023-05-31 07:57:52 +0800

    test(git): fix testing case

--- packages/git/src/__tests__/get-branch-commit-id.spec.ts
+++ packages/git/src/__tests__/get-branch-commit-id.spec.ts
@@ -3,7 +3,7 @@ import { getCommitIdOfBranch } from '../get-commit-id-of-branch.js';
 describe('getCommitIdOfBranch', () => {
   it('should correct last commit hash of branch', async () => {
     const hash = await getCommitIdOfBranch();
-    expect(hash.length).greaterThan(0);
+    expect(hash?.length).greaterThan(0);
     const hashworkspace = await getCommitIdOfBranch('workspace');
     expect(hashworkspace).toBe('a3f52eb46024e3275f906cb802d541f30506fb88');
   });
