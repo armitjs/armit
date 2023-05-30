7b8fea721716e6edc33d0e24cd785d5684c6bbbe
tianyingchun <tianyingchun@outlook.com>
2023-05-30 19:35:36 +0800

    test: fix test issue

--- packages/package/src/__tests__/get-external-modules.spec.ts
+++ packages/package/src/__tests__/get-external-modules.spec.ts
@@ -1,16 +1,17 @@
 import { getDirname } from '@armit/file-utility';
-import { getEsmExternalModules } from '../src/config-loader/loader-esm/get-external-modules.js';
+import { getPackageDependencyKeys } from '../package-dependency.js';
 
 describe('cosmiconfig get external modules', () => {
   const projectCwd = getDirname(import.meta.url);
   it('should load a valid TS file with named export', async () => {
-    const externalModules = await getEsmExternalModules(projectCwd);
+    const externalModules = await getPackageDependencyKeys(projectCwd);
     expect(externalModules).toEqual(
       expect.arrayContaining([
         '@armit/file-utility',
-        '@armit/package',
-        'cosmiconfig',
-        'type-fest',
+        '@armit/logger-node',
+        'as-table',
+        'boxen',
+        'dependency-tree',
       ])
     );
   });
