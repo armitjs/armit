4c249e1d1171f591ade7883e0b07176bbf82e92c
tianyingchun <tianyingchun@outlook.com>
2023-05-31 18:25:33 +0800

    test(git): fix testing case

--- packages/git/src/__tests__/commit-hash.spec.ts
+++ packages/git/src/__tests__/commit-hash.spec.ts
@@ -3,6 +3,6 @@ import { getLastCommitHash } from '../get-last-commit-hash.js';
 describe('getLastCommitHash', () => {
   it('should correct extract last commit hash', async () => {
     const hash = await getLastCommitHash();
-    expect(hash.length).toBe(7);
+    expect(hash?.length).toBe(7);
   });
 });
