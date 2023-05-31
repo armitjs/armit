bf9ec7fa994fcf527eec1ceb78a3542bed36764d
tianyingchun <tianyingchun@outlook.com>
2023-05-31 09:32:33 +0800

    test(git): fix test cases

--- packages/git/src/__tests__/get-branch-commit-id.spec.ts
+++ packages/git/src/__tests__/get-branch-commit-id.spec.ts
@@ -4,7 +4,7 @@ describe('getCommitIdOfBranch', () => {
   it('should correct last commit hash of branch', async () => {
     const hash = await getCommitIdOfBranch();
     expect(hash?.length).greaterThan(0);
-    const hashworkspace = await getCommitIdOfBranch('workspace');
-    expect(hashworkspace).toBe('a3f52eb46024e3275f906cb802d541f30506fb88');
+    const main = await getCommitIdOfBranch('main');
+    expect(main?.length).greaterThan(0);
   });
 });
