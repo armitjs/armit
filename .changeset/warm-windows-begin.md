67c4f99d4b446c0b1e21a4f43c8a1c7046d85c9e
tianyingchun <tianyingchun@outlook.com>
2023-06-17 14:35:03 +0800

    feat(git): add `remote` param for check branches contains commithash

---
+++ .changeset/warm-windows-begin.md
@@ -0,0 +1,5 @@
+---
+"@armit/git": patch
+---
+
+Add `remote` param for check branches contains commithash
--- packages/git/src/get-branches-between-commits.ts
+++ packages/git/src/get-branches-between-commits.ts
@@ -2,23 +2,26 @@ import { exec } from 'node:child_process';
 import { arrayUnique } from './array-unique.js';
 import { getAllCommitsBetween } from './get-all-commits-between.js';
 
-const getBranchesContainCommitHash = (commit: string) => {
+const getBranchesContainCommitHash = (commit: string, remote = true) => {
   return new Promise<string[]>((resolve) => {
-    exec(`git --no-pager branch --contains ${commit}`, (err, out) => {
-      if (err) {
-        // fatal: ambiguous argument 'df3s053': unknown revision or path not in the working tree.
-        return resolve([]);
-      }
-      const branches = out
-        .split(/[\r\n]/)
-        .filter((branch) => {
-          // Remove `* (HEAD detached at b121521)`
-          return !~branch.toUpperCase().indexOf('(HEAD');
-        })
-        .map((s) => s.trim());
+    exec(
+      `git --no-pager branch ${remote ? '-r' : ''} --contains ${commit}`,
+      (err, out) => {
+        if (err) {
+          // fatal: ambiguous argument 'df3s053': unknown revision or path not in the working tree.
+          return resolve([]);
+        }
+        const branches = out
+          .split(/[\r\n]/)
+          .filter((branch) => {
+            // Remove `* (HEAD detached at b121521)`
+            return !~branch.toUpperCase().indexOf('(HEAD');
+          })
+          .map((s) => s.trim());
 
-      resolve(branches);
-    });
+        resolve(branches);
+      }
+    );
   });
 };
 
@@ -26,16 +29,18 @@ const getBranchesContainCommitHash = (commit: string) => {
  * This will list all branches that contain any of the commits between commit1 and commit2
  * @param lastCommit if not will look up to first commit `HEAD`
  * @param earlyCommit The earlier commit `master`
+ * @param remote The remote-tracking
  * @returns All commits
  */
 export const getBranchesBetweenCommits = async (
   lastCommit: string,
-  earlyCommit: string
+  earlyCommit: string,
+  remote = true
 ) => {
   const allBranches: string[] = [];
   const allCommits = await getAllCommitsBetween(lastCommit, earlyCommit);
   for (const commit of allCommits) {
-    const branches = await getBranchesContainCommitHash(commit);
+    const branches = await getBranchesContainCommitHash(commit, remote);
     allBranches.push(...branches);
   }
   return arrayUnique(allBranches);
