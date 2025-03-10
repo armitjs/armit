6db3dad7a6212e8feae88dda6c1cb1dd4a7923d0
shunquan.wang <shunquan.wang@ly.com>
2025-03-10 18:40:53 +0800

    fix: fix case

--- packages/git/src/__tests__/get-branch-name.spec.ts
+++ packages/git/src/__tests__/get-branch-name.spec.ts
@@ -1,8 +1,12 @@
+import { execSync } from 'node:child_process';
 import { gitBranchName } from '../get-branch-name.js';
 
 describe('get-branch-name', () => {
   it('should correct extract branch name', async () => {
     const branchName = await gitBranchName();
-    expect(branchName).toBe(`main`);
+    const branch = execSync('git branch --show-current', {
+      encoding: 'utf-8',
+    }).trim();
+    expect(branchName).toBe(branch);
   });
 });
